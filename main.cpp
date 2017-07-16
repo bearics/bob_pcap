6fe35610fcd1b48dc4e095937a068e620bf10ed9
bearics <bearics@gmail.com>
2017-07-17 02:35:53 +0900

    Change function to see dump

--- main.cpp
+++ main.cpp
@@ -3,50 +3,6 @@
 #include <stdio.h>
 #include <netinet/in.h>
 
-void hexDump(void *addr, int len)
-{
-    int i;
-    unsigned char buff[17];
-    unsigned char *pc = (unsigned char*)addr;
-
-
-
-    // Process every byte in the data.
-    for (i = 0; i < len; i++) {
-        // Multiple of 16 means new line (with line offset).
-
-        if ((i % 16) == 0) {
-            // Just don't print ASCII for the zeroth line.
-            if (i != 0)
-                printf("  %s\n", buff);
-
-            // Output the offset.
-            printf("  %04x ", i);
-        }
-
-        // Now the hex code for the specific character.
-        printf(" %02x", pc[i]);
-
-        // And store a printable ASCII character for later.
-        if ((pc[i] < 0x20) || (pc[i] > 0x7e)) {
-            buff[i % 16] = '.';
-        } else {
-            buff[i % 16] = pc[i];
-        }
-
-        buff[(i % 16) + 1] = '\0';
-    }
-
-    // Pad out last line if not exactly 16 characters.
-    while ((i % 16) != 0) {
-        printf("   ");
-        i++;
-    }
-
-    // And print the final ASCII bit.
-    printf("  %s\n", buff);
-}
-
 int main(int argc, char *argv[])
 {
     pcap_t *handle;			/* Session handle */
@@ -106,7 +62,13 @@ int main(int argc, char *argv[])
         ltime=localtime(&local_tv_sec);
         strftime( timestr, sizeof timestr, "%H:%M:%S", ltime);
         printf("%s,%.6d len:%d\n", timestr, header->ts.tv_usec, header->len);
-        hexDump(&pkt_data, header->len);
+        for(int i=0;i<header->len;i++)
+        {
+            printf("%02x ", *(pkt_data+i));
+            if((i+1)%8==0) printf(" ");
+            if((i+1)%16==0) printf("\n");
+        }
+        printf("\n");
     }
 
     return(0);
