2255849e8bc60526c3de2acc64e45824566115ef
bearics <bearics@gmail.com>
2017-07-16 23:59:56 +0900

    Change pcap_next to pcap_next_ex

--- main.cpp
+++ main.cpp
@@ -11,8 +11,10 @@ int main(int argc, char *argv[])
     char filter_exp[] = "port 80";	/* The filter expression */
     bpf_u_int32 mask;		/* Our netmask */
     bpf_u_int32 net;		/* Our IP */
-    struct pcap_pkthdr header;	/* The header that pcap gives us */
+    struct pcap_pkthdr *header;	/* The header that pcap gives us */
     const u_char *packet;		/* The actual packet */
+    int res;
+    const u_char *pkt_data;
 
   //  struct in_addr addr;
 
@@ -46,9 +48,9 @@ int main(int argc, char *argv[])
     }
     while(1) {
         /* Grab a packet */
-        packet = pcap_next(handle, &header);
+        res = pcap_next_ex(handle,  &header, &pkt_data);
         /* Print its length */
-        printf("Jacked a packet with length of [%d]\n", header.len);
+        //printf("Jacked a packet with length of [%d]\n", res.len);
         /* And close the session */
         pcap_close(handle);
     }
